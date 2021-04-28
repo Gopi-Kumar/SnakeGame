30640687242ee7e3134faed517fe5ea80fe91bdb
Gopi Kumar Kaushik <gopikumarkaushik9065@gmail.com>
2021-04-28 13:57:11 +0530

    game ended board added

---
+++ board.js
@@ -0,0 +1,9 @@
+const gameEndedBoard = document.querySelector(".end_game_board");
+
+export function showGameEndedBoard(){
+    gameEndedBoard.style.display="block"
+}
+
+export function startNewGame(){
+    startNewGameButton
+}
--- game.js
+++ game.js
@@ -1,16 +1,15 @@
 import { update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection } from './snake.js'
 import { update as updateFood, draw as drawFood } from './food.js'
 import { outsideGrid } from './grid.js'
-
+import  { showGameEndedBoard, startNewGame } from './board.js'
 let lastRenderTime = 0
 let gameOver = false
 const gameBoard = document.getElementById('game-board')
 
 function main(currentTime) {
   if (gameOver) {
-    if (confirm('You lost. Press ok to restart.')) {
-      window.location = '/'
-    }
+    showGameEndedBoard();
+    
     return
   }
 
@@ -43,3 +42,8 @@ function draw() {
 function checkDeath() {
   gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
 }
+const startNewGameButton = document.querySelector("#startNewGame")
+
+startNewGameButton.addEventListener("click", ()=>{
+  window.location = '/';
+})
